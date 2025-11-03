c7787b7a5a31b0da5f65cae9c2027fe774a26f22
Julia Hay <jfh2152@columbia.edu>
2025-11-03 18:37:38 -0500

    Adding loop for different pages of options contracts

--- Tests/data_tests/optionsTest.py
+++ Tests/data_tests/optionsTest.py
@@ -1,4 +1,4 @@
-from polygon import RESTClient
+from massive import RESTClient
 import pandas as pd
 from pandas import json_normalize 
 import requests
@@ -32,8 +32,37 @@ order = 'asc'
 limit = 100
 sort_type = 'strike_price'
 
-r = requests.get(f"https://api.polygon.io/v3/reference/options/contracts?underlying_ticker={ticker}&contract_type={contract_type}&expired={expired}&order={order}&limit={limit}&sort={sort_type}&apiKey={API_KEY}")
+final_df = pd.DataFrame()
+url = "https://api.massive.com/v3/reference/options/contracts?contract_type=call&expired=false&order=asc&limit=1000&sort=ticker&apiKey=RkwOb2o8WMk16Vq_7_66LjDAcX_mXlgL"
+
+#https://api.massive.com/v3/reference/options/contracts?cursor=YXA9JTdCJTIySUQlMjIlM0ElMjI5OTI3NTQyNDk2MTg1NzI4OTYlMjIlMkMlMjJTdGFydERhdGVVdGMlMjIlM0ElN0IlMjJUaW1lJTIyJTNBJTIyMjAyNS0wNy0xNFQwMCUzQTAwJTNBMDBaJTIyJTJDJTIyVmFsaWQlMjIlM0F0cnVlJTdEJTJDJTIyRW5kRGF0ZVV0YyUyMiUzQSU3QiUyMlRpbWUlMjIlM0ElMjIwMDAxLTAxLTAxVDAwJTNBMDAlM0EwMFolMjIlMkMlMjJWYWxpZCUyMiUzQWZhbHNlJTdEJTJDJTIydW5kZXJseWluZ190aWNrZXIlMjIlM0ElMjJBQUwlMjIlMkMlMjJ0aWNrZXIlMjIlM0ElMjJPJTNBQUFMMjYwMzIwQzAwMDIzMDAwJTIyJTJDJTIyZXhwaXJhdGlvbl9kYXRlJTIyJTNBJTIyMjAyNi0wMy0yMFQwMCUzQTAwJTNBMDBaJTIyJTJDJTIyc3RyaWtlX3ByaWNlJTIyJTNBMjMlMkMlMjJjZmklMjIlM0ElMjJPQ0FTUFMlMjIlMkMlMjJjb250cmFjdF90eXBlJTIyJTNBJTIyY2FsbCUyMiUyQyUyMmV4ZXJjaXNlX3N0eWxlJTIyJTNBJTIyYW1lcmljYW4lMjIlMkMlMjJwcmltYXJ5X2V4Y2hhbmdlJTIyJTNBJTdCJTIyU3RyaW5nJTIyJTNBJTIyQkFUTyUyMiUyQyUyMlZhbGlkJTIyJTNBdHJ1ZSU3RCUyQyUyMnNoYXJlc19wZXJfY29udHJhY3QlMjIlM0ExMDAlMkMlMjJhZGRpdGlvbmFsX3VuZGVybHlpbmdzJTIyJTNBJTIyJTVCJTVEJTIyJTdEJmFzPSZjb250cmFjdF90eXBlPWNhbGwmZXhwaXJlZD1mYWxzZSZsaW1pdD0xMDAwJm9yZGVyPWFzYyZzb3J0PXRpY2tlcg&apiKey=RkwOb2o8WMk16Vq_7_66LjDAcX_mXlgL
+counter = 0
+
+while url is not None and counter < 5:
+    print(counter)
+    r = requests.get(url)
+    data = r.json()
+    if 'next_url' in data:
+        url = data['next_url'] + f'&apiKey={API_KEY}'
+        print("next_url:", url)
+    else:
+        url = None
+    if 'results' not in data:
+        print("No results found.")
+        break
+    df = pd.DataFrame(data["results"])
+    #print(df)
+    
+    final_df = pd.concat([final_df, df], ignore_index=True)
+    #print(final_df)
+    counter += 1
+
+final_df.index += 1
+print(final_df)
+"""
+r = requests.get(f"https://api.massive.com/v3/reference/options/contracts?expired=false&order=asc&limit=1000&sort=ticker&apiKey=RkwOb2o8WMk16Vq_7_66LjDAcX_mXlgL")
 data = r.json()
 df = pd.DataFrame(data["results"])
 df.index += 1
 print(df)
+"""
