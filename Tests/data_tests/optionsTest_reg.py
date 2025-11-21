1c56fb750ee97447b09ff0d8df2e68e775de83ea
EC2 Default User <ec2-user@ip-172-31-27-209.ec2.internal>
2025-11-21 02:02:18 +0000

    update

--- Tests/data_tests/optionsTest_reg.py
+++ Tests/data_tests/optionsTest_reg.py
@@ -12,7 +12,7 @@ client = RESTClient(api_key=API_KEY)
 
 
 ticker = 'TQQQ'
-date = '2025-11-11'
+date = '2025-11-17'
 
 """ 
 df = pd.DataFrame(client.list_options_contracts(
@@ -31,7 +31,7 @@ order = 'asc'
 limit = 1000
 sort_type = 'strike_price'
 
-final_df = pd.DataFrame()
+final_df = pd.DataFrame
 url = "https://api.massive.com/v3/snapshot?order=asc&ticker=" + ticker + "&limit=250&sort=ticker&apiKey=" + API_KEY
 
 counter = 0
@@ -39,7 +39,8 @@ counter = 0
 while url is not None and counter < 100:
     print(counter)
     r = requests.get(url)
-    data = r.json()
+    data = r
+    #print(data)
     if 'next_url' in data:
         url = data['next_url'] + f'&apiKey={API_KEY}'
     else:
@@ -48,14 +49,20 @@ while url is not None and counter < 100:
         print("No results found.")
         break
     df = pd.DataFrame(data["results"])
-    print(data)
+    print(df)
+   #print(df.to_string())
     
     final_df = pd.concat([final_df, df], ignore_index=True)
     #print(final_df)
     counter += 1
 
-final_df.index += 1
-print(final_df)
+#print(df.keys())
+
+
+# final_df.index += 1
+# print(final_df)
+# print(data)
+
 """
 r = requests.get(f"https://api.massive.com/v3/reference/options/contracts?expired=false&order=asc&limit=1000&sort=ticker&apiKey=RkwOb2o8WMk16Vq_7_66LjDAcX_mXlgL")
 data = r.json()
